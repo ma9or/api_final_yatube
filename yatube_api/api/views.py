194a99d8611d0fedb46fa50bb27cf0e5250357bf
Ахметжанов Ильдар <ma9or@mail.ru>
2022-03-30 10:31:33 +0600

    Спринт 9 попытка №6

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-
 from posts.models import Group, Post
 
 from .permissions import IsOwnerOrReadOnly
